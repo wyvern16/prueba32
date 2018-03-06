3bfe77197818a301a5dfe764d21f0b78af17818a
Laboratorio UNAM-Apple 7 <laboratoriounam-apple7@macbook-pro-de-laboratorio-7.local>
2018-03-06 09:11:08 -0600

    Initial Commit

---
+++ prueba32/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  prueba32
+//
+//  Created by Laboratorio UNAM-Apple 7 on 06/03/18.
+//  Copyright © 2018 Laboratorio Unam. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
